ce3ce8808201c932db41feefdb7f1a34c653120e
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-06-05 10:31:36 +0700

    feat:add register and logout controller

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $removeToken = $request->user()->tokens()->delete();
+
+        if ($removeToken) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Success!',
+            ]);
+        }
+    }
+}
---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+            'username' => ['required', 'unique:users,username'],
+            'nomer_telp' => ['required', 'numeric', 'digits_between:1,16'],
+            'tempat_lahir' => ['required', 'string'],
+            'tanggal_lahir' => ['required', 'date'],
+            'alamat' => ['required', 'string', 'max:255'],
+        ]);
+
+        $data = $request->all();
+        $data['unid'] = Str::uuid()->toString();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'message' => 'success registration',
+                'user' => $user
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
+}
