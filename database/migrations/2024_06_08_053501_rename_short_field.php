2bf1d03577ac355915f20b32be08cb64d4e3d4b6
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-06-09 21:25:18 +0700

    fix: change method for renaming column on rename migration

--- database/migrations/2024_06_08_053501_rename_short_field.php
+++ database/migrations/2024_06_08_053501_rename_short_field.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,7 +13,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('url', function (Blueprint $table) {
-            $table->renameColumn('`originalurl`', "shorturl");
+            $table->string('shorturl'); // Adjust the type and length according to your needs
+        });
+
+        DB::table('url')->update([
+            'shorturl' => DB::raw('originalurl')
+        ]);
+
+        Schema::table('url', function (Blueprint $table) {
+            $table->dropColumn('originalurl');
+        });
+
+        Schema::table('url', function (Blueprint $table) {
+            $table->string('shorturl')->change(); // Adjust the type and length if needed
         });
     }
 
@@ -21,6 +34,20 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('url', function (Blueprint $table) {
+            $table->string('shorturl'); // Adjust the type and length according to your needs
+        });
+
+        DB::table('url')->update([
+            'shorturl' => DB::raw('originalurl')
+        ]);
+
+        Schema::table('url', function (Blueprint $table) {
+            $table->dropColumn('originalurl');
+        });
+
+        Schema::table('url', function (Blueprint $table) {
+            $table->string('shorturl')->change(); // Adjust the type and length if needed
+        });
     }
 };
