6542c9f154fbc633f8ab43b9bfd43bea943fd518
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-06-09 21:11:26 +0700

    fix: add backticks on rename migration

--- database/migrations/2024_06_08_053501_rename_short_field.php
+++ database/migrations/2024_06_08_053501_rename_short_field.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('url', function (Blueprint $table) {
-            $table->renameColumn('originalurl', "shorturl");
+            $table->renameColumn('`originalurl`', "shorturl");
         });
     }
 
